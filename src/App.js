e917da4b70b44dc69590d6fa9a769c744096dbe2
code malayalam <nikithajoemon@gmail.com>
2024-11-28 15:00:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./component/header/Header.jsx";
+import Nav from "./component/Navigation/Nav.jsx";
+import Body from "./component/Body/Body.jsx";
 
 function App() {
   return (
     <div className="App">
-       hi
+      <Header />
+      <Nav />
+      <Body />
     </div>
   );
 }
